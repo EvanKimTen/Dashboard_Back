3a9b4e12b833857cedb57685d0ec3fea92f1e640
jhk2463 <125618020+jhk2463@users.noreply.github.com>
2023-09-16 18:37:13 -0400

    Query api connected

--- src/routes/knowledgebase.js
+++ src/routes/knowledgebase.js
@@ -7,8 +7,15 @@ const FormData = require("form-data");
 const router = express.Router();
 const DIALOG_MANAGER_API_KEY = process.env.DIALOG_MANAGER_API_KEY;
 
-const knowledgebaseApi = axios.create({
+const managementApi = axios.create({
   baseURL: "https://api.voiceflow.com/v3alpha/knowledge-base/docs",
+  headers: {
+    authorization: DIALOG_MANAGER_API_KEY,
+  },
+});
+
+const queryApi = axios.create({
+  baseURL: "https://general-runtime.voiceflow.com/knowledge-base/query",
   headers: {
     authorization: DIALOG_MANAGER_API_KEY,
   },
@@ -17,7 +24,7 @@ const knowledgebaseApi = axios.create({
 //Get all data sources
 router.get("/proxy/knowledge-base", async (req, res) => {
   try {
-    const response = await knowledgebaseApi.get("?Pagination=page=1&limit=50");
+    const response = await managementApi.get("?Pagination=page=1&limit=50");
     res.send(response.data);
   } catch (err) {
     console.error(err);
@@ -30,7 +37,7 @@ router.post("/proxy/knowledge-base", async (req, res) => {
   const { url } = req.body;
   console.log(url);
   try {
-    const response = await knowledgebaseApi.post("/upload", {
+    const response = await managementApi.post("/upload", {
       data: {
         type: "url",
         url: url,
@@ -53,7 +60,7 @@ router.post("/proxy/knowledge-base/file", upload.any(), async (req, res) => {
   const formFile = new FormData();
   formFile.append("file", buffer, { filename });
   try {
-    const response = await knowledgebaseApi.post("/upload", formFile);
+    const response = await managementApi.post("/upload", formFile);
     res.send(response.data);
   } catch (err) {
     console.error(err);
@@ -64,7 +71,29 @@ router.post("/proxy/knowledge-base/file", upload.any(), async (req, res) => {
 //Delete document
 router.delete("/proxy/knowledge-base/:documentID", async (req, res) => {
   try {
-    const response = await knowledgebaseApi.delete(`/${req.params.documentID}`);
+    const response = await managementApi.delete(`/${req.params.documentID}`);
+    res.send(response.data);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+//AI Preview
+router.post("/proxy/knowledge-base/preview", async (req, res) => {
+  const { question, settings } = req.body;
+  try {
+    const response = await queryApi.post(`/`, {
+      question: question,
+      chunkLimit: settings.chunkLimit,
+      synthesis: true,
+      settings: {
+        model: settings.model,
+        temperature: settings.temperature,
+        system: settings.system,
+      },
+    });
+    console.log(response);
     res.send(response.data);
   } catch (err) {
     console.error(err);
