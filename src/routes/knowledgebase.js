fee44c65e82a581dbf316462a83fa2d4d2d3fac7
jhk2463 <125618020+jhk2463@users.noreply.github.com>
2023-09-19 03:05:52 -0400

    Revert "Merge branch 'main' of https://github.com/hmjk99/Dashboard_Back"
    This reverts commit 62a297b5db9bb76233f5bf3dadbfc325df53e9b1, reversing
    changes made to ff841d422ad3a6085a081ae25aa37fc14ee7e4fe.

--- src/routes/knowledgebase.js
+++ src/routes/knowledgebase.js
@@ -1,49 +1,20 @@
 const express = require("express");
 const axios = require("axios");
-const multer = require("multer");
-const upload = multer();
-const FormData = require("form-data");
 
 const router = express.Router();
 const DIALOG_MANAGER_API_KEY = process.env.API_KEY;
 
-const managementApi = axios.create({
+const knowledgebaseApi = axios.create({
   baseURL: "https://api.voiceflow.com/v3alpha/knowledge-base/docs",
   headers: {
+    "Content-Type": "application/json",
     authorization: DIALOG_MANAGER_API_KEY,
   },
 });
 
-const queryApi = axios.create({
-  baseURL: "https://general-runtime.voiceflow.com/knowledge-base/query",
-  headers: {
-    authorization: DIALOG_MANAGER_API_KEY,
-  },
-});
-
-//Get all data sources
 router.get("/proxy/knowledge-base", async (req, res) => {
   try {
-    const response = await managementApi.get("?Pagination=page=1&limit=50");
-    res.send(response.data);
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ error: "Internal server error" });
-  }
-});
-
-//Upload a URL
-router.post("/proxy/knowledge-base", async (req, res) => {
-  const { url } = req.body;
-  console.log(url);
-  try {
-    const response = await managementApi.post("/upload", {
-      data: {
-        type: "url",
-        url: url,
-        name: url,
-      },
-    });
+    const response = await knowledgebaseApi.get("/");
     console.log(response.data);
     res.send(response.data);
   } catch (err) {
@@ -52,48 +23,18 @@ router.post("/proxy/knowledge-base", async (req, res) => {
   }
 });
 
-//Upload a File
-router.post("/proxy/knowledge-base/file", upload.any(), async (req, res) => {
-  const { files } = req;
-  const { buffer, originalname: filename } = files[0];
-
-  const formFile = new FormData();
-  formFile.append("file", buffer, { filename });
+router.post("/proxy/knowledge-base", async (req, res) => {
+  const { url } = req.body;
+  console.log(url);
   try {
-    const response = await managementApi.post("/upload", formFile);
-    res.send(response.data);
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ error: "Internal server error" });
-  }
-});
-
-//Delete document
-router.delete("/proxy/knowledge-base/:documentID", async (req, res) => {
-  try {
-    const response = await managementApi.delete(`/${req.params.documentID}`);
-    res.send(response.data);
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ error: "Internal server error" });
-  }
-});
-
-//AI Preview
-router.post("/proxy/knowledge-base/preview", async (req, res) => {
-  const { question, settings } = req.body;
-  try {
-    const response = await queryApi.post(`/`, {
-      question: question,
-      chunkLimit: settings.chunkLimit,
-      synthesis: true,
-      settings: {
-        model: settings.model,
-        temperature: settings.temperature,
-        system: settings.system,
+    const response = await knowledgebaseApi.post("/upload", {
+      data: {
+        type: "url",
+        url: url,
+        name: url,
       },
     });
-    console.log(response);
+    console.log(response.data);
     res.send(response.data);
   } catch (err) {
     console.error(err);
