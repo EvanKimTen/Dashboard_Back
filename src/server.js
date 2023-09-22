0cdec393a7acab8e2eefba0640e7f73a45f20e05
Moonjoo Kim <124408498+hmjk99@users.noreply.github.com>
2023-09-22 19:31:30 -0400

    fixed analytics and transcript

--- src/server.js
+++ src/server.js
@@ -13,18 +13,20 @@ app.use(cors());
 // Routes
 const analyticsRoutes = require("./routes/analytics");
 const knowledgebaseRoutes = require("./routes/knowledgebase");
+const transcriptRoutes = require("./routes/transcript");
 const userRoutes = require("./routes/user");
 
 app.use("/", analyticsRoutes);
 app.use("/", knowledgebaseRoutes);
 app.use("/", userRoutes);
+app.use("/", transcriptRoutes);
 
 mongoose
   .connect(process.env.MONGODB_URL)
   .then(() => console.log("Connected to mongodb"))
   .catch((err) => console.log(err));
 
-/////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 
 app.listen(port, () => {
   console.log(`Proxy server is running on port ${port}`);
